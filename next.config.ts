7e3bc5a4ba3e7bfcb46d7f096177aa4355d22aff
Abhimanyu Sharma <56254966+Abhisharma08@users.noreply.github.com>
2026-05-14 16:28:38 +0530

    base url updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  basePath: '/RetailBranding',
+  basePath: '/retail-branding',
   skipTrailingSlashRedirect: true,
   /* config options here */
   typescript: {
